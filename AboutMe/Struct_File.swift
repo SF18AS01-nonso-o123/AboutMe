9ff77f2827b4195e154ba9fad1a323f8dd4b2ff8
Chinonso <chinonsoobidike@chinonsos-mbp.bmcc.cuny.edu>
2019-03-28 14:47:50 -0400

    Initial commit

---
+++ AboutMe/Struct_File.swift
@@ -0,0 +1,15 @@
+//
+//  Struct_File.swift
+//  AboutMe
+//
+//  Created by Chinonso Obidike on 2/7/19.
+//  Copyright © 2019 Chinonso Obidike. All rights reserved.
+//
+
+import Foundation
+enum Sex: Character {
+    case male = "M";
+    case female = "F";
+    
+    
+}
